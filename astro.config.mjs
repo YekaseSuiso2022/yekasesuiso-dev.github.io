5f77f2631c1519a414ba59e09ed8136f99b42b03
YekaseSuiso🍥 <hydrogenneko@gmail.com>
2026-01-04 01:48:16 +0800

    Update site URL to include HTTPS

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "yekasesuiso-dev.github.io",
+	site: "https://yekasesuiso-dev.github.io",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
